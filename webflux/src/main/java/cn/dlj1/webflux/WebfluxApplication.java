d40fb4ce695862af22d3d32a23764f57f4bf60eb
fivewords <443672581@qq.com>
2019-07-05 18:53:39 +0800

    gateway

--- webflux/src/main/java/cn/dlj1/webflux/WebfluxApplication.java
+++ webflux/src/main/java/cn/dlj1/webflux/WebfluxApplication.java
@@ -4,19 +4,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class WebfluxApplication {
 
     public static void main(String[] args) {
-       new SpringApplication(WebfluxApplication.class).run(args);
+        new SpringApplication(WebfluxApplication.class).run(args);
     }
 
-    @GetMapping
-    public Mono<String> c(){
-        return Mono.empty();
+    @GetMapping("/")
+    public Mono<String> c() {
+        return Mono.just("你好!");
+    }
+
+    @GetMapping("/mono")
+    public Flux<List> mono() {
+        List<String> a = new ArrayList<>();
+        a.add("你好");
+        a.add("flux");
+        return Flux.just(a);
     }
 
 }
